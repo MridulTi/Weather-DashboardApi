5f0af8fa85ba332c9f0f7687c52493a0545d079a
= <mridultiwari2002@gmail.com>
2024-08-25 18:21:54 +0530

    changed proxy link

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-      '/api/v1':"https://wasserstoff-fullstack-intern-task-6nax-689jdty5f.vercel.app"
+      '/api/v1':"https://wasserstoff-fullstack-intern-task-46wb.vercel.app/"
     }
   }
 })
