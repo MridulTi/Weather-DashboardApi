5514ff7d1e41aeee954c8d6a98ceb967288fb506
= <mridultiwari2002@gmail.com>
2024-08-25 18:18:12 +0530

    Fixed Proxy issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-      '/api/v1':"http://localhost:8000"
+      '/api/v1':"https://wasserstoff-fullstack-intern-task-6nax-689jdty5f.vercel.app"
     }
   }
 })
