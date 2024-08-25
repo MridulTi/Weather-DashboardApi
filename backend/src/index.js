213cf4eed27a81cd11934a029dfd16080d66fa99
= <mridultiwari2002@gmail.com>
2024-08-25 18:16:21 +0530

    Added Vercel.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,28 +20,3 @@ connectDB()
   .catch((err)=>{
     console.error("MONGO DB Connection Failed !", err)
   });
-
-
-
-
-
-
-// Polluted the index file here
-/*import express from "express"
-const app=express()
-
-(async ()=>{
-  try{
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    app.on("error",(error)=>{
-      console.log("ERR:",error)
-      throw error
-    })
-    app.listen(process.env.PORT,()=>{
-      console.log(`App is listening on port : ${process.env.PORT}`)
-    })
-  }catch(error){
-    console.error("Error: ",error)
-    throw err
-  }
-})()*/
